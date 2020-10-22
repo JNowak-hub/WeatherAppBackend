aba0269985ced9575f8f126ac2f6cc3f03498abd
Jakub Nowak <nowakjakub095@gmail.com>
2020-10-22 10:24:33 +0200

    Removed role from UserEntityRequest

--- src/main/java/com/jakub/weather/model/dto/UserEntityRequest.java
+++ src/main/java/com/jakub/weather/model/dto/UserEntityRequest.java
@@ -1,16 +1,10 @@
 package com.jakub.weather.model.dto;
 
-import com.jakub.weather.model.user.Role;
-import java.util.ArrayList;
-import java.util.List;
-
 public class UserEntityRequest {
 
     private String userName;
 
     private String password;
-
-    private List<Role> role = new ArrayList<>();
 
 
     public String getUserName() {
@@ -27,14 +21,6 @@ public class UserEntityRequest {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public List<Role> getRole() {
-        return role;
-    }
-
-    public void setRole(List<Role> role) {
-        this.role = role;
     }
 
 }
