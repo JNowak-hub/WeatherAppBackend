ae0bbed71fb282ae80ee890ea05c235975b7441b
Jakub Nowak <nowakjakub095@gmail.com>
2020-10-28 10:46:40 +0100

    Changed native querry in settings repo to HQL

--- src/main/java/com/jakub/weather/repo/UserSettingsEntiyRepo.java
+++ src/main/java/com/jakub/weather/repo/UserSettingsEntiyRepo.java
@@ -10,9 +10,11 @@ import java.util.Optional;
 @Repository
 public interface UserSettingsEntiyRepo extends JpaRepository<UserSettingsEntity, Long> {
 
-    @Query(value = "DELETE FROM user_settings_entity AS us JOIN user_entity AS ue ON ue.settings_id = us.id where ue.id = ?1 ", nativeQuery = true)
+//    @Query(value = "DELETE FROM user_settings_entity AS us JOIN user_entity AS ue ON ue.settings_id = us.id where ue.id = ?1 ", nativeQuery = true)
+    @Query(value = "DELETE FROM UserSettingsEntity us WHERE us.user.id = :userId")
     void deleteUserSettings(Long userId);
 
-    @Query(value = "SELECT us.* FROM user_settings_entity AS us JOIN user_entity AS ue ON ue.settings_id = us.id where ue.id = ?1", nativeQuery = true)
+//    @Query(value = "SELECT us.* FROM user_settings_entity AS us JOIN user_entity AS ue ON ue.settings_id = us.id where ue.id = ?1", nativeQuery = true)
+    @Query(value = "SELECT us FROM UserSettingsEntity us WHERE us.user.id = :id")
     Optional<UserSettingsEntity> getUserSettingsByUserId(Long id);
 }
